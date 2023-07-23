0c2bf3ac64f04f2f3399e514bfa5fc4d4cd893a9
Oleksandr Shuryhin <shuryhin.oleksandr@gmail.com>
2023-07-24 01:02:11 +0300

    Adjust expertise section

--- src/sections/_digital-brother/landing/digital-brother-landing-services.js
+++ src/sections/_digital-brother/landing/digital-brother-landing-services.js
@@ -5,13 +5,11 @@ import Card from '@mui/material/Card';
 import Stack from '@mui/material/Stack';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
-import IconButton from '@mui/material/IconButton';
 
-import { paths } from 'src/routes/paths';
-import Iconify from 'src/components/iconify';
+import {paths} from 'src/routes/paths';
 import SvgColor from 'src/components/svg-color';
-import { RouterLink } from 'src/routes/components';
 import TextMaxLine from 'src/components/text-max-line';
+import Image from "../../../components/image";
 
 // ----------------------------------------------------------------------
 
@@ -20,7 +18,7 @@ const COLORS = ['primary', 'secondary', 'success', 'warning'];
 const SERVICES = [
   {
     name: 'Healthcare',
-    icon: '/assets/icons/service/ic_service_seo.svg',
+    icon: '/assets/images/expertise/healthcare.png',
     content:
       'We create appointment system for medical organizations. ' +
       'Payments, analysis storage and export, analytics and alarms are included.',
@@ -28,7 +26,7 @@ const SERVICES = [
   },
   {
     name: 'Marketing',
-    icon: '/assets/icons/service/ic_service_mail.svg',
+    icon: '/assets/images/expertise/marketing.png',
     content:
       'We integrate with Google, Facebook, Instagram marketing API, ' +
       'implement account and ads management system.',
@@ -36,7 +34,7 @@ const SERVICES = [
   },
   {
     name: 'Telecommunications',
-    icon: '/assets/icons/service/ic_service_analysis.svg',
+    icon: '/assets/images/expertise/telecommunications.png',
     content:
       'We created software for billing for 1000+ headcount telecommunications companies. ' +
       'We calculate metrics for multiple tenants in parallel.',
@@ -56,26 +54,26 @@ export default function DigitalBrotherLandingServices() {
   return (
     <Container
       sx={{
-        py: { xs: 5, md: 10 },
+        py: {xs: 5, md: 10},
       }}
     >
       <Stack
         spacing={3}
         sx={{
           maxWidth: 480,
-          mb: { xs: 8, md: 5 },
-          mx: { xs: 'auto', md: 'unset' },
-          textAlign: { xs: 'center', md: 'unset' },
+          mb: {xs: 8, md: 5},
+          mx: {xs: 'auto', md: 'unset'},
+          textAlign: {xs: 'center', md: 'unset'},
         }}
       >
-        <Typography variant="overline" sx={{ color: 'text.disabled' }}>
+        <Typography variant="overline" sx={{color: 'text.disabled'}}>
           Our Expertise
         </Typography>
 
         <Typography variant="h2">Industries</Typography>
 
-        <Typography sx={{ color: 'text.secondary' }}>
-          Nunc nonummy metus. Donec elit libero, sodales nec, volutpat a, suscipit non, turpis.
+        <Typography sx={{color: 'text.secondary'}}>
+          We are well-known and have a good understanding of Django libraries in following industries:
         </Typography>
       </Stack>
 
@@ -92,7 +90,7 @@ export default function DigitalBrotherLandingServices() {
         }}
       >
         {SERVICES.map((service, index) => (
-          <ServiceItem key={service.name} service={service} index={index} />
+          <ServiceItem key={service.name} service={service} index={index}/>
         ))}
       </Box>
     </Container>
@@ -101,37 +99,40 @@ export default function DigitalBrotherLandingServices() {
 
 // ----------------------------------------------------------------------
 
-function ServiceItem({ service, index }) {
-  const { name, icon, content, path } = service;
+function ServiceItem({service, index}) {
+  const {name, icon, content, path} = service;
 
   return (
     <Card
       sx={{
         px: 4,
-        py: 5,
+        py: 3,
         textAlign: 'center',
         ...(index === 1 && {
-          py: { xs: 5, md: 8 },
+          py: {xs: 3, md: 6},
         }),
         ...(index === 2 && {
-          py: { xs: 5, md: 10 },
-          boxShadow: (theme) => ({ md: theme.customShadows.z24 }),
+          py: {xs: 3, md: 8},
+          boxShadow: (theme) => ({md: theme.customShadows.z24}),
         }),
       }}
     >
-      <SvgColor
+      <Image
+        visibleByDefault
+        disabledEffect
+        alt="marketing market"
         src={icon}
+        ratio="1/1"
         sx={{
+          borderRadius: 1.5,
           width: 88,
           height: 88,
-          mx: 'auto',
-          color: (theme) => theme.palette[COLORS[index]].main,
         }}
       />
 
-      <Stack spacing={1} sx={{ my: 5 }}>
+      <Stack spacing={1} sx={{my: 5}}>
         <TextMaxLine variant="h6">{name}</TextMaxLine>
-        <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+        <Typography variant="body2" sx={{color: 'text.secondary'}}>
           {content}
         </Typography>
       </Stack>
