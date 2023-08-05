e2f67643349fdf209d1290de8f44294a40775733
Oleksandr Shuryhin <shuryhin.oleksandr@gmail.com>
2023-08-05 23:49:31 +0300

    Fix imports

--- src/sections/_digital-brother/view/digital-brother-landing-view.js
+++ src/sections/_digital-brother/view/digital-brother-landing-view.js
@@ -1,25 +1,13 @@
 'use client';
 
-import {
-  _brands,
-  _members,
-  _marketingPosts,
-  _pricingMarketing,
-} from 'src/_mock';
-
-import MarketingTeam from "../../_marketing/team/marketing-team";
 import MarketingNewsletter from "../../_marketing/marketing-newsletter";
-import DigitalBrotherAboutOurClients from "../digital-brother-technologies";
-import PricingMarketing from "../../pricing/marketing/pricing-marketing";
+import DigitalBrotherTechnologies from "../digital-brother-technologies";
 import DigitalBrotherLandingHero from "../landing/digital-brother-landing-hero";
-import BlogMarketingLatestPosts from "../../blog/marketing/marketing-latest-posts";
 import DigitalBrotherTestimonial from "../testimonial/digital-brother-testimonial";
-import MarketingLandingFaqs from "../../_marketing/landing/marketing-landing-faqs";
 import DigitalBrotherLandingServices from "../landing/digital-brother-landing-services";
 import DigitalBrotherServicesInclude from "../services/digital-brother-services-include";
 import MarketingLandingFreeSEO from "../../_marketing/landing/marketing-landing-free-seo";
 import DigitalBrotherLandingCaseStudies from "../landing/digital-brother-landing-case-studies";
-import DigitalBrotherTechnologies from "../digital-brother-technologies";
 
 // ----------------------------------------------------------------------
 
@@ -30,21 +18,13 @@ export default function DigitalBrotherLandingView() {
 
       <DigitalBrotherLandingServices/>
 
-      <DigitalBrotherTestimonial />
+      <DigitalBrotherTestimonial/>
 
       <DigitalBrotherServicesInclude/>
 
       <DigitalBrotherLandingCaseStudies/>
 
       <DigitalBrotherTechnologies/>
-
-      <MarketingTeam members={_members}/>
-
-      <PricingMarketing plans={_pricingMarketing}/>
-
-      <MarketingLandingFaqs/>
-
-      <BlogMarketingLatestPosts posts={_marketingPosts.slice(0, 4)}/>
 
       <MarketingLandingFreeSEO/>
 
