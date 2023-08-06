36ce21ced9bdf010e2a2f5f4219ddc823d8f93ac
Oleksandr Shuryhin <shuryhin.oleksandr@gmail.com>
2023-08-07 00:45:11 +0300

    Fix footer

--- src/layouts/digital-brother/footer.js
+++ src/layouts/digital-brother/footer.js
@@ -2,23 +2,8 @@ import Stack from '@mui/material/Stack';
 import Divider from '@mui/material/Divider';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
-import {alpha, styled} from '@mui/material/styles';
-import Button, {buttonClasses} from '@mui/material/Button';
 import Logo from 'src/components/logo';
 import {usePathname} from 'src/routes/hooks';
-
-// ----------------------------------------------------------------------
-
-const StyledAppStoreButton = styled(Button)(({theme}) => ({
-  flexShrink: 0,
-  padding: '5px 12px',
-  color: theme.palette.common.white,
-  border: `solid 1px ${alpha(theme.palette.common.black, 0.24)}`,
-  background: `linear-gradient(180deg, ${theme.palette.grey[900]} 0%, ${theme.palette.common.black} 100%)`,
-  [`& .${buttonClasses.startIcon}`]: {
-    marginLeft: 0,
-  },
-}));
 
 // ----------------------------------------------------------------------
 
