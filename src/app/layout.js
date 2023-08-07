63bee24fd98eb111fa129c088130a0fcab211a3d
Oleksandr Shuryhin <shuryhin.oleksandr@gmail.com>
2023-08-07 22:50:51 +0300

    Update favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -23,24 +23,24 @@ export const metadata = {
   icons: [
     {
       rel: 'icon',
-      url: '/favicon/favicon.ico',
+      url: '/favicon/favicon.svg',
     },
     {
       rel: 'icon',
       type: 'image/png',
       sizes: '16x16',
-      url: '/favicon/favicon-16x16.png',
+      url: '/favicon/favicon.svg',
     },
     {
       rel: 'icon',
       type: 'image/png',
       sizes: '32x32',
-      url: '/favicon/favicon-32x32.png',
+      url: '/favicon/favicon.svg',
     },
     {
       rel: 'apple-touch-icon',
       sizes: '180x180',
-      url: '/favicon/apple-touch-icon.png',
+      url: '/favicon/favicon.svg',
     },
   ],
 };
